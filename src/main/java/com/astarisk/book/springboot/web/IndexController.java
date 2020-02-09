6027ac0ffa60819f9ff72fb732d51b19f28061d3
lee.jaesung <lee.jaesung@joins.com>
2020-02-10 08:40:57 +0900

    page296

--- src/main/java/com/astarisk/book/springboot/web/IndexController.java
+++ src/main/java/com/astarisk/book/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.astarisk.book.springboot.web;
 
+import com.astarisk.book.springboot.config.auth.LoginUser;
+import com.astarisk.book.springboot.config.auth.dto.SessionUser;
+import com.astarisk.book.springboot.domain.user.User;
 import com.astarisk.book.springboot.service.PostsService;
 import com.astarisk.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private  final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+           model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
