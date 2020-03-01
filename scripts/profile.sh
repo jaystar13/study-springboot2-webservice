84e6fd4711d449b5ea594c889690864c68bd4963
lee.jaesung <lee.jaesung@joins.com>
2020-03-02 08:51:29 +0900

    무중단배포스크립트 만들기 - 스크립트 정비

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
+
 # 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 
 function find_idle_profile()
@@ -27,7 +29,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == real ]
+    if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
     else
--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR name: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
